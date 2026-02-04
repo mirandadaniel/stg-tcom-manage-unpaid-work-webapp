96dafd29fd433134c5dd1bad230bcee1746250c2
mirandadaniel <mirandalily99@yahoo.co.uk>
2026-02-04 09:54:59 +0000

    aligned subheader with header

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </header>
         <div className="govuk-phase-banner">
           <p className="govuk-phase-banner__content">
-            <strong className="govuk-tag govuk-phase-banner__content__tag">PROTOTYPE</strong><span className="govuk-phase-banner__text">This is a prototype.</span>
+            <strong className="govuk-tag govuk-phase-banner__content__tag">PROTOTYPE</strong>
+            <span className="govuk-phase-banner__text">This is a prototype.</span>
           </p>
         </div>
         {children}
